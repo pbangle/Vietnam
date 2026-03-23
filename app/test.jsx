0ec1d2e8e397d6d474858c0c0b17f6877d17311d
pbangle <61518001+pbangle@users.noreply.github.com>
2026-03-23 17:33:21 +0100

    s

---
+++ app/test.jsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+  return (
+    <main style={{ padding: 40, color: "white", background: "#111", minHeight: "100vh" }}>
+      Test page works
+    </main>
+  );
+}
